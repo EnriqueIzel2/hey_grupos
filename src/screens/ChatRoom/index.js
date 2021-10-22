872945ec20295eb96a1eea7f9f5c4cb721801a8e
EnriqueIzel2 <eleaoizel@gmail.com>
2021-10-21 21:20:07 -0400

    feat: add style

--- src/screens/ChatRoom/index.js
+++ src/screens/ChatRoom/index.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 // import { Container } from './styles';
 
 const ChatRoom = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Chat Room Top</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default ChatRoom;
