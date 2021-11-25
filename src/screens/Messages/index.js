27cfb560e7dcc2784710deb52b6fe9355723200f
EnriqueIzel2 <eleaoizel@gmail.com>
2021-11-24 21:32:28 -0400

    feat: create the effect to bring the message data
    the effect is listening to the messages while the component is assembling, when the component is disassembled the listener stops

--- src/screens/Messages/index.js
+++ src/screens/Messages/index.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
 
-// import { Container } from './styles';
+const Messages = ({ route }) => {
+  const { thread } = route.params;
+  const [messages, setMessages] = useState([]);
+  const user = auth().currentUser.toJSON();
 
-const Messages = () => {
+  useEffect(() => {
+    const unsubscribeListener = firestore()
+      .collection("MESSAGE_THREADS")
+      .doc(thread._id)
+      .collection("MESSAGES")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((querySnapshot) => {
+        const messages = querySnapshot.docs.map((doc) => {
+          const firebaseData = doc.data();
+          const data = {
+            _id: doc.id,
+            text: "",
+            createdAt: firestore.FieldValue.serverTimestamp(),
+            ...firebaseData,
+          };
+
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.diplayName,
+            };
+          }
+
+          return data;
+        });
+
+        setMessages(messages);
+      });
+
+    return () => unsubscribeListener();
+  }, []);
+
   return (
     <View>
       <Text>Mensagens tops</Text>
