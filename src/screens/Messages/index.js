10e284b01183fd63aac3bf559d5c455949ca71fb
EnriqueIzel2 <eleaoizel@gmail.com>
2021-12-08 11:01:55 -0400

    feat: create the input message and function to send message

--- src/screens/Messages/index.js
+++ src/screens/Messages/index.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, FlatList, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import auth from "@react-native-firebase/auth";
 import firestore from "@react-native-firebase/firestore";
+import Feather from "react-native-vector-icons/Feather";
+
 import ChatMessage from "../../components/ChatMessage";
 
 const Messages = ({ route }) => {
   const { thread } = route.params;
-  const [messages, setMessages] = useState([]);
   const user = auth().currentUser.toJSON();
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     const unsubscribeListener = firestore()
@@ -41,6 +54,44 @@ const Messages = ({ route }) => {
     return () => unsubscribeListener();
   }, []);
 
+  async function handleSendMessage() {
+    console.log("handleSendMessage");
+    if (input.length <= 0) {
+      console.log("campo vazio");
+      return;
+    }
+
+    await firestore()
+      .collection("MESSAGE_THREADS")
+      .doc(thread._id)
+      .collection("MESSAGES")
+      .add({
+        text: input,
+        createdAt: firestore.FieldValue.serverTimestamp(),
+        user: {
+          _id: user.uid,
+          displayName: user.displayName,
+        },
+      });
+
+    await firestore()
+      .collection("MESSAGE_THREADS")
+      .doc(thread._id)
+      .set(
+        {
+          lastMessage: {
+            text: input,
+            createdAt: firestore.FieldValue.serverTimestamp(),
+          },
+        },
+        {
+          merge: true,
+        }
+      );
+
+    setInput("");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -48,7 +99,33 @@ const Messages = ({ route }) => {
         data={messages}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => <ChatMessage data={item} />}
+        inverted={true}
       />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ width: "100%" }}
+        keyboardVerticalOffset={100}
+      >
+        <View style={styles.containerInput}>
+          <View style={styles.mainContainerInput}>
+            <TextInput
+              placeholder="Mensagem"
+              style={styles.textInput}
+              value={input}
+              onChangeText={(text) => setInput(text)}
+              multiline={true}
+              autoCorrect={false}
+            />
+          </View>
+
+          <TouchableOpacity onPress={handleSendMessage}>
+            <View style={styles.buttonContainer}>
+              <Feather name="send" size={22} color="#fff" />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -58,6 +135,33 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  containerInput: {
+    flexDirection: "row",
+    margin: 10,
+    alignItems: "flex-end",
+  },
+  mainContainerInput: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  textInput: {
+    flex: 1,
+    marginHorizontal: 10,
+    maxHeight: 130,
+    minHeight: 48,
+  },
+  buttonContainer: {
+    backgroundColor: "#51c880",
+    height: 48,
+    width: 48,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25,
   },
 });
 
