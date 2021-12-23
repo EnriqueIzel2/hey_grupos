0e1593f137e64e3422089c00d36e449c76c98ac2
EnriqueIzel2 <eleaoizel@gmail.com>
2021-12-23 19:21:21 -0400

    feat: add input to search for chat room
    add components and style

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,14 +1,64 @@
-import React from "react";
-import { Text, View } from "react-native";
-
-// import { Container } from './styles';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+
   return (
-    <View>
-      <Text>O Search</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder="Digite o nome da sala"
+          value={search}
+          onChangeText={(text) => setSearch(text)}
+          style={styles.input}
+          autoCapitalize="none"
+        />
+
+        <TouchableOpacity style={styles.buttonSearch}>
+          <MaterialIcons name="search" size={30} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  containerInput: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+    marginVertical: 14,
+  },
+  input: {
+    backgroundColor: "#EBEBEB",
+    marginLeft: 10,
+    height: 50,
+    width: "80%",
+    borderRadius: 4,
+    padding: 5,
+  },
+  buttonSearch: {
+    backgroundColor: "#2e54e4",
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "15%",
+    marginLeft: 5,
+    marginRight: 10,
+  },
+});
 
 export default Search;
